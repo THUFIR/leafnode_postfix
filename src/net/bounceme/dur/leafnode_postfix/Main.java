1d0fa0060e70e6b3779e018215527392b4b3568a
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-08-25 18:57:33 -0700

    increment

--- src/net/bounceme/dur/leafnode_postfix/Main.java
+++ src/net/bounceme/dur/leafnode_postfix/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package dur.bounceme.net.leafnode_postfix;
+package net.bounceme.dur.leafnode_postfix;
 
 /**
  *
