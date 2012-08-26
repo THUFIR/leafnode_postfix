ff08aed1566a2e82d660f250b4238820dd4f4f4d
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-08-26 00:55:30 -0700

    increment

--- src/net/bounceme/dur/leafnode_postfix/Main.java
+++ src/net/bounceme/dur/leafnode_postfix/Main.java
@@ -3,17 +3,20 @@ package net.bounceme.dur.leafnode_postfix;
 import static java.lang.System.out;
 import java.util.List;
 import javax.mail.Folder;
+import javax.mail.MessagingException;
 
 public class Main {
 
     private Usenet u = Usenet.INSTANCE;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MessagingException {
         Main m = new Main();
     }
 
-    private Main() {
+    private Main() throws MessagingException {
         List<Folder> folders = u.getFolders();
         out.println(folders);
+        MailClient mc = new MailClient("user", "host");
+        mc.sendMessage("thufir@dur", "some subj", "no content");
     }
 }
