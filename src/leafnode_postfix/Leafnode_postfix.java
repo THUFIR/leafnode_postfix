0434b20d09f507b7211a88b32b528a5a6a2cc5c1
THUFIR <HAWAT.THUFIR@GMAIL.COM>
2012-08-25 18:41:54 -0700

    increment

---
+++ src/leafnode_postfix/Leafnode_postfix.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leafnode_postfix;
+
+/**
+ *
+ * @author thufir
+ */
+public class Leafnode_postfix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
